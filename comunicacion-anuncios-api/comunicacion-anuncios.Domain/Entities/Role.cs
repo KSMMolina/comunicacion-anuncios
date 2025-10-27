fa73c0c81bec58355bab965fe22642d8ed18fb95
KSMMolina <kmartinez@ofima.com>
2025-10-27 12:48:14 -0500

    Delete comunicacion-anuncios-api directory

--- comunicacion-anuncios-api/comunicacion-anuncios.Domain/Entities/Role.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace comunicacion_anuncios.Domain.Entities
-{
-    public sealed class Role
-    {
-        public Guid Id { get; private set; }
-        public string Name { get; private set; } = default!;
-        public ICollection<User> Users { get; } = new List<User>();
-    }
-}
